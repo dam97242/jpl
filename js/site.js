8c9e757178a12f380460e919942bc76b067c65f0
dam97242 <diogomota99@hotmail.com>
2017-03-08 15:53:57 +0000

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
 $(function () { $("#Hide").on("click", function () { $(this).replaceWith("E-mail: " + "geral" + "@" + "jpl" + "-" + "contabilidade" + "." + "pt") }) })
-$(function () { $("#Hidetele").on("click", function () { $(this).replaceWith("Telefone: " + "9110" + "213"+"22") }) })
+$(function () { $("#Hidetelf").on("click", function () { $(this).replaceWith("Telefone: " + "9110" + "213"+"22") }) })
 $(function () { $("#Hidetele").on("click", function () { $(this).replaceWith("Telemóvel: " + "9110" + "213"+"22") }) })
 $(function () { $("#Hiderua").on("click", function () { $(this).replaceWith("Loja: " + "Rua Marquês " + "de Pombal " + "Loja G " + "nº19" + " -Salvaterra de " + "Magos") }) })
