24582181f7cab70c9c099fa9cead66ce038674b1
dam97242 <diogomota99@hotmail.com>
2020-06-05 22:06:48 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
 $(function () { $("#Hide").on("click", function () { $(this).replaceWith("E-mail: " + "geral" + "@" + "jpl" + "-" + "contabilidade" + "." + "pt") }) })
 $(function () { $("#Hidetelf").on("click", function () { $(this).replaceWith("Telefone: " + "263" + "505"+"028") }) })
 $(function () { $("#Hidetele").on("click", function () { $(this).replaceWith("Telemóvel: " + "9110" + "213"+"22") }) })
-$(function () { $("#Hiderua").on("click", function () { $(this).replaceWith("Loja: " + "Rua Marquês " + "de Pombal " + "n.19 " + "loja G" + " -Salvaterra de " + "Magos") }) })
+$(function () { $("#Hiderua").on("click", function () { $(this).replaceWith("Loja: " + "Rua Urbanização " + "das acácias " + "n.35 " + " -Marinhais " ) }) })
