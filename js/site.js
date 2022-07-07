e32660a59d204229324b251904b21c0446954770
dam97242 <diogomota99@hotmail.com>
2022-07-07 12:53:07 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-$(function () { $("#Hide").on("click", function () { $(this).replaceWith("E-mail: " + "geral" + "@" + "jpl" + "-" + "contabilidade" + "." + "pt") }) })
+$(function () { $("#Hide").on("click", function () { $(this).replaceWith("E-mail: " + "jpl.contabilidade" + "@" + "gmail" + "." + "com") }) })
 $(function () { $("#Hidetelf").on("click", function () { $(this).replaceWith("Telefone: " + "263" + "505"+"028") }) })
 $(function () { $("#Hidetele").on("click", function () { $(this).replaceWith("Telemóvel: " + "9110" + "213"+"22") }) })
-$(function () { $("#Hiderua").on("click", function () { $(this).replaceWith("Loja: " + "Praceta Urbanização " + "das acácias " + "n.35 anexo " + " -Marinhais " ) }) })
+$(function () { $("#Hiderua").on("click", function () { $(this).replaceWith("Loja: " + "Rua do " + "Tanoeiro " + "n.42 anexo " + "2125-149 -Marinhais " ) }) })
