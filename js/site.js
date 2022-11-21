d5081532225fc67b5f6662f85413cbce874bc4c4
dam97242 <diogomota99@hotmail.com>
2022-11-21 12:30:05 +0000

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
 $(function () { $("#Hide").on("click", function () { $(this).replaceWith("E-mail: " + "jpl.contabilidade" + "@" + "gmail" + "." + "com") }) })
-$(function () { $("#Hidetelf").on("click", function () { $(this).replaceWith("Telefone: " + "263" + "505"+"028") }) })
-$(function () { $("#Hidetele").on("click", function () { $(this).replaceWith("Telemóvel: " + "9110" + "213"+"22") }) })
+$(function () { $("#Hidetelf").on("click", function () { $(this).replaceWith("Telefone: " + "263" + "505"+"028"+"(rede fixa nacional)") }) })
+$(function () { $("#Hidetele").on("click", function () { $(this).replaceWith("Telemóvel: " + "9110" + "213"+"22"+"(rede móvel nacional)") }) })
 $(function () { $("#Hiderua").on("click", function () { $(this).replaceWith("Loja: " + "Rua do " + "Tanoeiro " + "n.42 anexo " + "2125-149 -Marinhais " ) }) })
